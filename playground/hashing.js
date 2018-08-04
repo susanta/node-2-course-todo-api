c87b8aa17908271fb561a1c2b7e6fd74c71518e7
Susanta <inbox.susanta@gmail.com>
2018-08-05 01:59:38 +0530

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 11
+};
+
+var token = jwt.sign(data, '123Hash');
+console.log(token);
+
+var decoded = jwt.verify(token, '123Hash');
+console.log('decoded', decoded);
+
+
+// var message = "Hey Susanta";
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     // data : data,
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if( resultHash === token.hash ) {
+//     console.log('data was not changed');
+// } else {
+//     console.log('Data was changed. Do not trust!');
+// }
