47b51284b9874df97c7a0d329199374e0c781723
Susanta <inbox.susanta@gmail.com>
2018-08-05 13:48:06 +0530

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -39,6 +39,7 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email']);
 }
 
+// instance methods
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth'; 
@@ -50,6 +51,31 @@ UserSchema.methods.generateAuthToken = function () {
         return token;
     });
 };
+
+// model meethods
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+        
+        // same as above
+        return Promise.reject();
+    }
+
+    return User.findOne({
+       '_id': decoded._id,
+       'tokens.token': token,
+       'tokens.access': 'auth'
+       
+    });
+};
+
 
 var User = mongoose.model('User', UserSchema);
 
