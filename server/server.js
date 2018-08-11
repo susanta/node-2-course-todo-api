ab2932e8c6f2c3f63392a29e31e31e6600386500
Susanta <inbox.susanta@gmail.com>
2018-08-11 12:14:09 +0530

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -96,6 +96,7 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -111,6 +112,21 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+//  POST /users/login
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.send(400).send();
+    });
+
+    
 });
 
 app.listen(port, () => {
